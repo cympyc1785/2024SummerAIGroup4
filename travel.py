0224f60af4773659cf9672c75452e0eb6f6d572a
giantoctopus <2022100021@korea.ac.kr>
2024-08-06 15:22:36 +0900

    add place info

--- travel.py
+++ travel.py
@@ -1,16 +1,26 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
+from geopy.geocoders import Nominatim
 
-image = Image.open(r'./사진/KakaoTalk_2022-06-04 14-06-00.jpg')
+import datetime
+from glob import glob
+import os
+
+image = Image.open(r'./사진/KakaoTalk_20240806_151540574.jpg')
 info = image._getexif() #이미지 메타정보
-info
 
 
+# GPSInfo 정보 추출(위도, 경도)
 from PIL.ExifTags import TAGS
 for tag,value in info.items():
-    # print(value)
     decoded = TAGS.get(tag,tag) #태그숫자를 디코딩
-    print(tag,decoded,value)
 
+    if decoded == 'GPSInfo':
+        gps_lat =  value.get(2) #위도
+        gps_lon =  value.get(4) #경도
+        lat = (((gps_lat[2] / 60.0 ) +  gps_lat[1]) / 60.0) + gps_lat[0]
+        lon = (((gps_lon[2] / 60.0 ) +  gps_lon[1]) / 60.0) + gps_lon[0]
+        lat_lng_str = f'{lat},{lon}' #위도 경도 문자열
+        loc = geolocator.reverse(lat_lng_str)
+        loc.address
 
-    

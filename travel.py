ef1ddb9b766e576d612409bc36342e0267786b63
giantoctopus <2022100021@korea.ac.kr>
2024-08-06 15:47:34 +0900

    time add (incomplete)

--- travel.py
+++ travel.py
@@ -1,14 +1,12 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
-from geopy.geocoders import Nominatim
 
 import datetime
 from glob import glob
 import os
 
-image = Image.open(r'./사진/KakaoTalk_20240806_151540574.jpg')
+image = Image.open(r'.\python\2024SummerAIGroup4\KakaoTalk_20240806_151540574.jpg')
 info = image._getexif() #이미지 메타정보
-
 
 # GPSInfo 정보 추출(위도, 경도)
 from PIL.ExifTags import TAGS
@@ -18,9 +16,15 @@ for tag,value in info.items():
     if decoded == 'GPSInfo':
         gps_lat =  value.get(2) #위도
         gps_lon =  value.get(4) #경도
-        lat = (((gps_lat[2] / 60.0 ) +  gps_lat[1]) / 60.0) + gps_lat[0]
-        lon = (((gps_lon[2] / 60.0 ) +  gps_lon[1]) / 60.0) + gps_lon[0]
-        lat_lng_str = f'{lat},{lon}' #위도 경도 문자열
-        loc = geolocator.reverse(lat_lng_str)
-        loc.address
+        lat = (((gps_lat[2] / 60.0 ) +  gps_lat[1]) / 60.0) + gps_lat[0] #위도 단위 통일
+        lon = (((gps_lon[2] / 60.0 ) +  gps_lon[1]) / 60.0) + gps_lon[0] #경도 단위 통일
+
+    if decoded == 'DateTime':
+       dt = datetime.datetime.strptime(value,"%Y:%m:%d %H:%M:%S")
+       dtime = dt.strftime("%Y-%m-%d %H-%M-%S")
+       print(dtime)
+        
+        
+
+
 

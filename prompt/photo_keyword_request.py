6db52305cb2b59ea2a91df94ba6fab829162f505
the-apple <90840239+the-apple@users.noreply.github.com>
2024-08-06 17:22:42 +0900

    Resizing이 포함된 photo_keyword_request

--- prompt/photo_keyword_request.py
+++ prompt/photo_keyword_request.py
@@ -3,6 +3,8 @@ import requests
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from PIL import Image
+from io import BytesIO
 
 env_path = Path('..')/'.env'
 load_dotenv(dotenv_path=env_path)
@@ -10,8 +12,19 @@ api_key = os.getenv("API_KEY")
 image_path = "./sample.png"
 
 def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+    with Image.open(image_path) as image_file:
+        width, height = image_file.size
+        # Resize the image to 512 x 512
+        if width > 512:
+            image_file = image_file.resize((512,height))
+            width = 512
+        if height > 512:
+            image_file = image_file.resize((width, 512))
+            height = 512
+       # Save the image to a BytesIO object 
+        buffered = BytesIO()
+        image_file.save(buffered, format="PNG")
+        return base64.b64encode(buffered.getvalue()).decode('utf-8')
 
 base64_image = encode_image(image_path)
 
@@ -46,3 +59,4 @@ payload = {
 response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
 print(response.json())
+#print(response.json())

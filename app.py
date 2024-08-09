c185a3a22be988314baa4360e2e376debf46dcd1
cympyc1785 <68377988+cympyc1785@users.noreply.github.com>
2024-08-09 14:20:33 +0900

    MOD

--- app.py
+++ app.py
@@ -11,35 +11,33 @@ import os
 import tempfile
 import webbrowser
 import pandas as pd
-import streamlit as st
-
 def goto_link(site_url):
     webbrowser.open(site_url)
-
-# load .env
+# Load .env
 load_dotenv()
 API_KEY = os.environ.get('API_KEY')
-
-# 아무거나 초기설정
+# Initial Setup
 geolocator = Nominatim(user_agent='geoapigroup4')
-
 if 'playlist_generated' not in st.session_state:
     st.session_state.playlist_generated = False
-
 if 'music_filled' not in st.session_state:
     st.session_state.music_filled = False
-
 if 'prev_image' not in st.session_state:
     st.session_state.prev_image = None
-
 if 'prev_description' not in st.session_state:
     st.session_state.prev_description = ""
-
-
-st.image('https://bcassetcdn.com/social/vm77minaoy/preview.png', width = 200)
-
-tab1, tab2 = st.tabs(['사용자 정보 입력',"서비스 소개 및 사용 가이드"])
-
+if 'total_playlists' not in st.session_state:
+    st.session_state.total_playlists = []
+if 'travel_title' not in st.session_state:
+    st.session_state.travel_title = ""
+st.title('TripTunes')
+tab1, tab2 = st.tabs(['사용자 정보 입력', "서비스 소개 및 사용 가이드"])
+# Sidebar update function
+def update_sidebar():
+    with st.sidebar:
+        for title in st.session_state.total_playlists:
+            st.write(title)
+#update_sidebar()  # Initial sidebar update
 with tab1:
     st.title('당신은 어떤 사람인가요?')
     gender = st.text_input('당신의 성별을 입력해주세요.')
@@ -47,23 +45,17 @@ with tab1:
     character = st.text_input("당신의 MBTI를 입력해주세요.")
     genre = st.multiselect(
         '선호하는 장르를 선택해주세요.',
-        ['Pop','Rock','Jazz','Classic', 'Hip-hop', 'R&B/Soul', 'Electronic','Blues'])
+        ['Pop', 'Rock', 'Jazz', 'Classic', 'Hip-hop', 'R&B/Soul', 'Electronic', 'Blues'])
     st.write('You Selected:', ','.join(genre))
-
     if st.button('플레이리스트 생성'):
         st.session_state.playlist_generated = True
-
     if st.session_state.playlist_generated:
-    
-        st.title('당신의 여행을 표현해주세요 ✈️')
-    
-        st.write("여행 중 찍은 가장 좋아하는 사진을 올려주세요 📷")
-        image = st.file_uploader("사진 첨부", type = ['png','jpeg','jpg'])
+        st.title('당신의 여행을 표현해주세요 :비행기:')
+        st.write("여행 중 찍은 가장 좋아하는 사진을 올려주세요 :카메라:")
+        image = st.file_uploader("사진 첨부", type=['png', 'jpeg', 'jpg'])
         description = st.text_input("사진에 대한 설명을 넣어주세요.")
-        
-        if image is not None and description is not None:
+        if image is not None:
             st.image(image)
-
             if image != st.session_state.prev_image or description != st.session_state.prev_description:
                 st.session_state.prev_image = image
                 st.session_state.prev_description = description
@@ -71,27 +63,13 @@ with tab1:
                 temp_dir = tempfile.mkdtemp()
                 img_path = os.path.join(temp_dir, image.name)
                 with open(img_path, "wb") as f:
-                        f.write(image.getvalue())
-
+                    f.write(image.getvalue())
                 caption = pkr.get_image_caption(img_path)
-
-                print("Caption : ", caption)
-
                 loc, dtime = get_image_metadata(img_path)
                 img_meta_data = (loc, dtime)
-
                 user_data = {'gender': gender, 'age': age, 'character': character, 'genre': genre}
-                
                 st.session_state.recommendation = pkr.get_recommendation(caption, description, img_meta_data, user_data)
-
-                print("Recommendations : ", st.session_state.recommendation)
-
-            recomm = [['"Bubble Pop"', '', 'Hyuna', '', 'K-pop'], ['"Electric Feel"', '', 'MGMT  ', '', 'Psychedelic Rock'], ['"Dog Days Are Over"', '', 'Florence + The Machine', '', 'Indie Rock'], ['"Go!"', '', 'The Chemical Brothers (feat. Q-Tip)', '', 'Electronic Dance Music'], ['"Happy"', '', 'Pharrell Williams', '', 'Pop'], ['"Daft Punk Is Playing At My House"', '', 'LCD Soundsystem', '', 'Dance Punk'], ['"Shake It Off"', '', 'Taylor Swift', '', 'Pop'], ['"Experiment On Me"', '', 'Halsey', '', 'Alternative/Indie'], ['"Blue Monday"', '', 'New Order', '', 'Synthpop'], ['"Viva La Vida"', '', 'Coldplay', '', 'Alternative Rock'], ['"I Got A Boy"', '', "Girls' Generation", '', 'K-pop'], ['"Do It"', '', 'Chloe x Halle', '', 'R&B/Soul']]
-
-            rec_v2 = [['"Dynamite"', 'BTS', 'K-pop'], ['"Blinding Lights"', 'The Weeknd', 'Synthwave/Pop'], ['"Watermelon Sugar"', 'Harry Styles', 'Pop'], ['"Electric Feel"', 'MGMT', 'Indietronica/Psychedelic pop'], ['"Little Dark Age"', 'MGMT', 'Synthpop/Indietronica'], ['"Stay"', 'The Kid LAROI & Justin Bieber', 'Pop'], ['"Slide"', 'Calvin Harris ft. Frank Ocean & Migos', 'Electropop'], ['"On Our Way"', 'The Royal Concept', 'Indie Rock/Indietronica'], ['"You Can Call Me Al"', 'Paul Simon', 'Worldbeat/Pop Rock'], ['"Deja Vu"', 'Olivia Rodrigo', 'Pop/Rock'], ['"A Boy Is a Gun"', 'Tyler, the Creator', 'Alternative Hip-Hop'], ['"Phantom Pt. II"', 'Justice', 'Electro House']]
-
             col1, col2 = st.columns(2)
-            
             style = """
             <style>
             .title {
@@ -109,16 +87,12 @@ with tab1:
             <p class="title">Recommendation</p>
             """
             playlist1_subheader = """
-            <p class="subheader">이 세상 하나뿐인 플레이리스트와 함께 여행해보세요 🎶</p>
+            <p class="subheader">이 세상 하나뿐인 플레이리스트와 함께 여행해보세요 :음표2:</p>
             """
             playlist2_subheader = """
             <p class="subheader">당신의 취향에 맞는 노래들로 채워보세요.</p>
             """
-
-            #st.markdown(html_code, unsafe_allow_html=True)
-
             selected_music = []
-
             with col1:
                 st.markdown(playlist2_title, unsafe_allow_html=True)
                 st.markdown(playlist2_subheader, unsafe_allow_html=True)
@@ -142,37 +116,25 @@ with tab1:
                         hide_index=True
                     )
                     selected_music = editable_df[editable_df["Select"] == True]
-                
             with col2:
-                st.markdown(style+playlist1_title, unsafe_allow_html=True)
-                st.markdown(style+playlist1_subheader, unsafe_allow_html=True)
+                st.markdown(style + playlist1_title, unsafe_allow_html=True)
+                st.markdown(style + playlist1_subheader, unsafe_allow_html=True)
                 if len(selected_music) > 0:
                     st.dataframe(selected_music.drop(columns=["Select"]),
                                  column_config={
                                      "URL": st.column_config.LinkColumn("Music URL")
                                  }, hide_index=True, width=700)
-                # Display the selected music
-                #st.write("Selected Music:")
-                #st.write(selected_music)
-                #st.title('당신만의 플레이리스트')
-                #st.subheader('이 세상 하나뿐인 플레이리스트와 함께 여행해보세요 🎶')
-                
-
-            #if st.button('음악 채우기'):
-            #    st.session_state.music_filled = True
-            
-            if st.session_state.music_filled:
+            # Separated title input and button logic
+            st.session_state.travel_title = st.text_input("여행의 제목을 입력해주세요")
+            if st.button('음악 채우기') and st.session_state.travel_title:
+                st.session_state.total_playlists.append(st.session_state.travel_title)
+                update_sidebar()  # Update sidebar after adding new playlist title
                 st.success('음악이 당신의 플레이리스트에 채워졌습니다!')
-
 with tab2:
     st.title('TripTunes')
-    
+    st.image('https://dynamic.design.com/preview/logodraft/6ee31420-5ed6-412e-87b0-d1e4d35bcce2/image/large.png', width=300)
     st.write('저희의 새로운 서비스 Triptunes는 여러분들의 여행을 더 풍요롭게 만들 음악 플레이리스트를 제공합니다.Triptunes와 함께 여행을 떠나보세요.')
-
     st.title('서비스 이용 가이드')
     st.write('1. 사용자 정보를 입력해주세요')
     st.write('2. 선호하는 음악 장르를 선택해주세요.')
     st.write('3. 당신의 여행을 표현한 글 혹은 이미지를 자유롭게 선택해주세요.')
-    
-    
-    
--- prompt/photo_keyword_request.py
+++ prompt/photo_keyword_request.py
@@ -6,13 +6,11 @@ from pathlib import Path
 from PIL import Image
 from io import BytesIO
 from openai import OpenAI
-
 def get_api_key():
     env_path = '.env'
     load_dotenv(dotenv_path=env_path)
     api_key = os.environ.get('API_KEY')
     return api_key
-
 def encode_image(image_path):
     with Image.open(image_path) as image_file:
         width, height = image_file.size
@@ -23,21 +21,17 @@ def encode_image(image_path):
         if height > 512:
             image_file = image_file.resize((width, 512))
             height = 512
-       # Save the image to a BytesIO object 
+       # Save the image to a BytesIO object
         buffered = BytesIO()
         image_file.save(buffered, format="PNG")
         return base64.b64encode(buffered.getvalue()).decode('utf-8')
-
 def get_image_caption(image_path):
     api_key = get_api_key()
-
     base64_image = encode_image(image_path)
-
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}"
     }
-
     payload = {
         "model": "gpt-4o-mini",
         "messages": [
@@ -57,19 +51,15 @@ def get_image_caption(image_path):
                         "image_url": {
                             "url": f"data:image/jpeg;base64, {base64_image}"
                         }
-
                     }
                 ]
             }
         ],
         "max_tokens": 300
     }
-
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-
     return response.json()['choices'][0]['message']['content']
     #print(response.json())
-
 def get_recommendation(caption, description, img_meta_data, user_data):
     # tools = [
     #     {
@@ -90,24 +80,20 @@ def get_recommendation(caption, description, img_meta_data, user_data):
     #         },
     #     }
     # ]
-
     api_key = get_api_key()
-
     # Create an OpenAI client.
     client = OpenAI(api_key=api_key)
-
     prompt = "Keywords : " + caption + "\n"
     prompt += "Description : " + description + "\n"
     prompt += "Location : " + str(img_meta_data[0]) + "\n"
     prompt += "Date Time : " + str(img_meta_data[1]) + "\n"
     prompt += """Using these as the context, recommend me 12 musics.
-            Try providing results from various domains, movie osts, K-pop ... etc. Be creative.
+            Try providing results from various genre. But do not stray from the context. Be creative.
             Your answer should only include title, singer, genre like
             "Running Up That Hill"\tChromatics\tSynthpop/Indietronica\n"134340"\tBTS\tK-pop\n ...
             with only \t, \n and no comma or period
             """
-
-    # Generate a response using the OpenAI API.         
+    # Generate a response using the OpenAI API.
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
@@ -125,12 +111,9 @@ def get_recommendation(caption, description, img_meta_data, user_data):
             {"role": "user", "content": prompt}
             ],
     )
-
     content = response.choices[0].message.content
     parsed_list = content.split('\n')
     parsed_list = [cont.split('\t') for cont in parsed_list]
-
     if len(parsed_list) < 1 or len(parsed_list[0]) < 3:
         return get_recommendation(caption, description, img_meta_data, user_data)
-
     return parsed_list
